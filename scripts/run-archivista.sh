e49a7dca8f09eb8ff01aa76870b5f6af94279c91
Frederick Kautz <fkautz@alumni.cmu.edu>
2023-12-16 16:14:05 -0800

    scripts to run in systemd on aws
    Signed-off-by: Frederick Kautz <fkautz@alumni.cmu.edu>

---
+++ scripts/run-archivista.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+DB_SECRET_VALUE=$(aws secretsmanager --region=us-east-1 get-secret-value --secret-id "$ARCHIVISTA_DB_SECRET_ARN")
+
+DATABASE_USER="$(echo $DB_SECRET_VALUE | jq -r '.SecretString | fromjson | .username')"
+DATABASE_PASSWORD="$(echo $DB_SECRET_VALUE | jq -r '.SecretString | fromjson | .password')"
+DATABASE_HOST="$(echo $DB_SECRET_VALUE | jq -r '.SecretString | fromjson | .host')"
+DATABASE_DBNAME="$(echo $DB_SECRET_VALUE | jq -r '.SecretString | fromjson | .dbname')"
+
+export ARCHIVISTA_LISTEN_ON="tcp://0.0.0.0:8082"
+export ARCHIVISTA_SQL_STORE_CONNECTION_STRING="$DATABASE_USER:$DATABASE_PASSWORD@tcp($DATABASE_HOST)/$DATABASE_DBNAME"
+export ARCHIVISTA_SQL_STORE_BACKEND='MYSQL'
+export ARCHIVISTA_STORAGE_BACKEND='BLOB'
+export ARCHIVISTA_BLOB_STORE_ENDPOINT='s3.amazonaws.com'
+export ARCHIVISTA_BLOB_STORE_BUCKET_NAME="hellocdkstack-archivistabucket4ebcf6df-tid7xehs2vuv"
+
+/home/ec2-user/go/bin/archivista
